50124136453c32af773504a7c652aaf53e8d339c
JasonK369 <17778562+JasonK369@users.noreply.github.com>
2025-03-21 04:31:52 +0000

    exclude null field

--- src/main/java/com/checkout/payment/gateway/model/ErrorResponse.java
+++ src/main/java/com/checkout/payment/gateway/model/ErrorResponse.java
@@ -1,15 +1,17 @@
 package com.checkout.payment.gateway.model;
 
+import com.checkout.payment.gateway.enums.PaymentStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
   private final String message;
 
-  public ErrorResponse(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return message;
-  }
+  private PaymentStatus paymentStatus;
 
   @Override
   public String toString() {
