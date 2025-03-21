ec12249cbbb127775c4911b0d7d48a21605bf3a9
JasonK369 <17778562+JasonK369@users.noreply.github.com>
2025-03-21 00:37:07 +0000

    implement Processing a payment logic

--- src/main/java/com/checkout/payment/gateway/service/PaymentGatewayService.java
+++ src/main/java/com/checkout/payment/gateway/service/PaymentGatewayService.java
@@ -1,31 +1,102 @@
 package com.checkout.payment.gateway.service;
 
+import com.checkout.payment.gateway.enums.PaymentStatus;
 import com.checkout.payment.gateway.exception.EventProcessingException;
+import com.checkout.payment.gateway.exception.InvalidInformationException;
+import com.checkout.payment.gateway.model.AuthoriseRequest;
+import com.checkout.payment.gateway.model.AuthoriseResponse;
 import com.checkout.payment.gateway.model.PostPaymentRequest;
 import com.checkout.payment.gateway.model.PostPaymentResponse;
 import com.checkout.payment.gateway.repository.PaymentsRepository;
+import java.util.Currency;
 import java.util.UUID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static utils.CreditCardInformationValidator.isCardExpired;
+
 @Service
+@Slf4j
 public class PaymentGatewayService {
 
-  private static final Logger LOG = LoggerFactory.getLogger(PaymentGatewayService.class);
+  @Autowired
+  private PaymentHandlingService paymentHandlingService;
 
-  private final PaymentsRepository paymentsRepository;
+  @Autowired
+  private PaymentsRepository paymentsRepository;
 
-  public PaymentGatewayService(PaymentsRepository paymentsRepository) {
-    this.paymentsRepository = paymentsRepository;
-  }
+//  public PaymentGatewayService(PaymentsRepository paymentsRepository) {
+//    this.paymentsRepository = paymentsRepository;
+//  }
 
   public PostPaymentResponse getPaymentById(UUID id) {
-    LOG.debug("Requesting access to to payment with ID {}", id);
+    log.debug("Requesting access to to payment with ID {}", id);
     return paymentsRepository.get(id).orElseThrow(() -> new EventProcessingException("Invalid ID"));
   }
 
-  public UUID processPayment(PostPaymentRequest paymentRequest) {
-    return UUID.randomUUID();
+  public PostPaymentResponse processPayment(PostPaymentRequest postPaymentRequest) {
+    // validation
+    validationPaymentInformation(postPaymentRequest);
+
+    // payment
+    AuthoriseResponse authoriseResponse = paymentHandlingService.authorisePayment(
+        mapAuthoriseRequest(postPaymentRequest));
+
+    // save to db
+    PostPaymentResponse postPaymentResponse = mapPostPaymentResponse(postPaymentRequest,
+        authoriseResponse);
+    paymentsRepository.add(postPaymentResponse);
+
+    return postPaymentResponse;
+  }
+
+  private AuthoriseRequest mapAuthoriseRequest(PostPaymentRequest postPaymentRequest) {
+
+    return new AuthoriseRequest(
+        postPaymentRequest.getCardNumber(),
+        postPaymentRequest.getExpiryDate(),
+        postPaymentRequest.getCurrency(),
+        postPaymentRequest.getAmount(),
+        postPaymentRequest.getCvv()
+    );
+  }
+
+  private PostPaymentResponse mapPostPaymentResponse(PostPaymentRequest postPaymentRequest,
+      AuthoriseResponse authoriseResponse) {
+    return new PostPaymentResponse(
+        UUID.randomUUID(),
+        authoriseResponse.getAuthorized() ? PaymentStatus.AUTHORIZED : PaymentStatus.DECLINED,
+        trimCreditCardNumber(postPaymentRequest.getCardNumber(), 4),
+        postPaymentRequest.getExpiryMonth(),
+        postPaymentRequest.getExpiryYear(),
+        postPaymentRequest.getCurrency(),
+        postPaymentRequest.getAmount()
+    );
+  }
+
+  private void validationPaymentInformation(PostPaymentRequest postPaymentRequest) {
+    checkCardExpired(postPaymentRequest.getExpiryMonth(), postPaymentRequest.getExpiryYear());
+
+    checkCurrency(postPaymentRequest.getCurrency());
+
+  }
+
+  private void checkCardExpired(int expiryMonth, int expiryYear) {
+    if (isCardExpired(expiryMonth, expiryYear)) {
+      throw new InvalidInformationException("Card expired");
+    }
+  }
+
+  private void checkCurrency(String currency) {
+    try {
+      Currency.getInstance(currency);
+    } catch (IllegalArgumentException illegalArgumentException) {
+      throw new InvalidInformationException("Invalid currency");
+    }
+  }
+
+  private String trimCreditCardNumber(String creditCardNumber, int digitsToDisplay) {
+    return creditCardNumber.substring(creditCardNumber.length() - digitsToDisplay);
   }
 }
