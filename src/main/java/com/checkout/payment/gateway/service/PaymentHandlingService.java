d1c50b83d890df70e0ec6673a22c5c532fb7a7bd
JasonK369 <17778562+JasonK369@users.noreply.github.com>
2025-03-21 03:31:37 +0000

    use endpoint from config

--- src/main/java/com/checkout/payment/gateway/service/PaymentHandlingService.java
+++ src/main/java/com/checkout/payment/gateway/service/PaymentHandlingService.java
@@ -7,10 +7,12 @@ import com.checkout.payment.gateway.model.AuthoriseRequest;
 import com.checkout.payment.gateway.model.AuthoriseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -20,23 +22,29 @@ public class PaymentHandlingService {
   @Autowired
   RestTemplate restTemplate;
 
-  public AuthoriseResponse authorisePayment(AuthoriseRequest authoriseRequest){
+  @Value("${bank.base-url}")
+  String baseUrl;
+
+  @Value("${bank.payment-endpoint}")
+  String paymentEndpoint;
+
+  public AuthoriseResponse authorisePayment(AuthoriseRequest authoriseRequest) {
     log.info("Incoming request = {}", authoriseRequest);
 
     try {
-      return restTemplate.postForEntity("localhost:8080/payments", authoriseRequest,
+      return restTemplate.postForEntity(baseUrl + paymentEndpoint, authoriseRequest,
           AuthoriseResponse.class).getBody();
-    }catch(HttpClientErrorException httpClientErrorException){
+    } catch (HttpClientErrorException httpClientErrorException) {
       HttpStatusCode httpStatusCode = httpClientErrorException.getStatusCode();
       String errorResponse = httpClientErrorException.getResponseBodyAsString();
 
       if (httpStatusCode.value() == HttpStatus.BAD_REQUEST.value()) {
         throw new MissingPaymentInformationException(errorResponse);
-      } else if (httpStatusCode.value() == HttpStatus.SERVICE_UNAVAILABLE.value()) {
-        throw new BankServerUnavailableException(errorResponse);
       }
 
       throw new PaymentUnsuccessfulException(errorResponse);
+    } catch (HttpServerErrorException httpServerErrorException) {
+      throw new BankServerUnavailableException("Bank unavailable, no payment made");
     }
   }
 
