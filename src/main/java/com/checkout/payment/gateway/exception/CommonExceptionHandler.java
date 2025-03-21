d6a1a03b8ea34a32257440d95e0c68522706a6f9
JasonK369 <17778562+JasonK369@users.noreply.github.com>
2025-03-21 03:30:49 +0000

    add handler method for bank unavailable

--- src/main/java/com/checkout/payment/gateway/exception/CommonExceptionHandler.java
+++ src/main/java/com/checkout/payment/gateway/exception/CommonExceptionHandler.java
@@ -5,8 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class CommonExceptionHandler {
@@ -18,5 +22,23 @@ public class CommonExceptionHandler {
     LOG.error("Exception happened", ex);
     return new ResponseEntity<>(new ErrorResponse("Page not found"),
         HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(BankServerUnavailableException.class)
+  public ResponseEntity<ErrorResponse> handleBankServerUnavailable(
+      BankServerUnavailableException bankServerUnavailableException){
+    return new ResponseEntity<>(new ErrorResponse(bankServerUnavailableException.getMessage()),
+        HttpStatus.BAD_GATEWAY);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 }
